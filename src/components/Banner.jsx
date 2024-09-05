0081c26ee6a0268455e6e9fa24cf8a57cece613c
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-05 16:03:16 +0100

    implements banner module and tidies css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,9 @@
+import styles from "./Banner.module.css"
 import Nav from "./Nav";
 
 const Banner = ({ setVisibleSection }) => {
     return (
-        <div className="Banner">
+        <div className={`Banner ${styles.Banner}`}>
             <Nav setVisibleSection={setVisibleSection} />
         </div>
     );
