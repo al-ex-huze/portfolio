b8b6f51b6b11f7d7ecbc7e70862b69d61a5e266b
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-05 16:31:41 +0100

    implements disappearing banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,36 @@
-import styles from "./Banner.module.css"
+import styles from "./Banner.module.css";
 import Nav from "./Nav";
+import { useEffect, useState } from "react";
 
 const Banner = ({ setVisibleSection }) => {
+    const [isVisible, setIsVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY) {
+            console.log("SCL Y ? LAST Y")
+            setIsVisible(false);
+        } else {
+            setIsVisible(true);
+        }
+        setLastScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
+
     return (
-        <div className={`Banner ${styles.Banner}`}>
+        <div
+            className={`${styles.Banner} ${
+                isVisible ? "Banner__visible" : "Banner__hidden"
+            }`}
+        >
             <Nav setVisibleSection={setVisibleSection} />
         </div>
     );
