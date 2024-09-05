2c76b2588d5de502da356d3915532acdf4d6ab3e
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-05 19:30:11 +0100

    create contact module

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,13 @@
 import "../App.css";
+
 import ContactCard from "./ContactCard";
+
+import styles from "./Contact.module.css"
 
 const Contact = ({ visibleSection }) => {
     return (
         <div
-            className={`Contact section ${
+            className={`${styles.Contact}${" "}Contact__${
                 visibleSection === "section3" ? "fade-in" : "fade-out"
             }`}
             id="section3"
