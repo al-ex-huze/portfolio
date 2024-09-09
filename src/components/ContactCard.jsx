7cae6d720bc5d4493f6f498de24515c3798f2c01
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-09 13:18:43 +0100

    adds photo to contact

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,6 +3,7 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -11,11 +12,25 @@ import { IconButton } from "@mui/material";
 
 const ContactCard = () => {
     return (
-        <Box sx={{ marginTop: "5px", minWidth: 200, display: "flex", justifySelf: "center"}}>
+        <Box
+            sx={{
+                marginTop: "5px",
+                minWidth: 200,
+                display: "flex",
+                justifySelf: "center",
+            }}
+        >
             <Card variant="outlined">
                 <CardContent>
                     <Typography variant="h5">Get In Touch</Typography>
                 </CardContent>
+                <CardMedia
+                    style={{ paddingTop: "100%", borderRadius: "50%" }}
+                    image={
+                        "https://alimageexbuckhuetzepub.s3.eu-north-1.amazonaws.com/ah-hs-1.jpg"
+                    }
+                    title="Headshot"
+                />
                 <CardActions
                     sx={{
                         display: "flex",
@@ -93,6 +108,6 @@ const ContactCard = () => {
             </Card>
         </Box>
     );
-}
+};
 
 export default ContactCard;
