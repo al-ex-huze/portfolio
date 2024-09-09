e74bfcec8807bc2e24f32c87e3e01d025cb0c32b
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-09 07:44:40 +0100

    adds modes to pagination controls

--- src/components/ProfileSwiper.jsx
+++ src/components/ProfileSwiper.jsx
@@ -1,15 +1,20 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import Typography from "@mui/material/Typography";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Parallax, Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+
 import styles from "./ProfileSwiper.module.css";
 
-import { Autoplay, Parallax, Pagination, Navigation } from "swiper/modules";
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const ProfileSwiper = () => {
+    const { theme } = useContext(ThemeContext);
+    console.log(JSON.stringify(theme));
+
     return (
         <div className={`${styles.ProfileSwiper}`}>
             <Swiper
@@ -18,11 +23,19 @@ const ProfileSwiper = () => {
                     delay: 3000,
                     disableOnInteraction: true,
                 }}
-                style={{
-                    "--swiper-navigation-color": "#fff",
-                    "--swiper-pagination-color": "#fff",
-                    padding: "10px",
-                }}
+                style={
+                    theme.palette.mode === "dark"
+                        ? {
+                              "--swiper-navigation-color": "#ffffff",
+                              "--swiper-pagination-color": "#ffffff",
+                              padding: "10px",
+                          }
+                        : {
+                              "--swiper-navigation-color": "#000d15",
+                              "--swiper-pagination-color": "#000d15",
+                              padding: "10px",
+                          }
+                }
                 speed={600}
                 parallax={true}
                 pagination={{
@@ -48,9 +61,7 @@ const ProfileSwiper = () => {
                         className={`${styles.subtitle_xs}`}
                         data-swiper-parallax="-300"
                     >
-                        <Typography sx={{ color: "theme.primary" }}>
-                            Hi, I'm
-                        </Typography>
+                        Hi, I'm...
                     </div>
                     <div className={`${styles.title}`} data-swiper-parallax="0">
                         Alex Hughes
