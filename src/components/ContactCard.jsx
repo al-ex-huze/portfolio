69d4ad0056be96df2bb43c0dad751848fd01b756
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-13 13:33:46 +0100

    changes projects cards to normal ones

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -10,16 +10,36 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import { IconButton } from "@mui/material";
 
+import ReactFlipCard from "reactjs-flip-card";
+
+
 const ContactCard = () => {
+    const rfcStyles = {
+        card: {
+            color: "white",
+            borderRadius: 20,
+        },
+    };
+
     return (
         <Box
             sx={{
                 marginTop: "5px",
+                marginBottom: "10px",
                 minWidth: 200,
                 display: "flex",
                 justifySelf: "center",
             }}
         >
+            <ReactFlipCard
+                    flipTrigger="onHover"
+                    containerStyle={{
+                        width: "500px",
+                        height: "300px",
+                    }}
+                    frontStyle={rfcStyles.card}
+                    backStyle={rfcStyles.card}
+                    frontComponent={
             <Card variant="outlined">
                 <CardContent>
                     <Typography variant="h5">Get In Touch</Typography>
@@ -99,6 +119,88 @@ const ContactCard = () => {
                     {"("}#Open To Work{")"}
                 </Typography>
             </Card>
+             }
+             backComponent={
+                <Card variant="outlined">
+                <CardContent>
+                    <Typography variant="h5">Get In Touch</Typography>
+                </CardContent>
+                <CardActions
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                        width: "fit-content",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <IconButton
+                        aria-label="redirect"
+                        onClick={(event) =>
+                            (window.location.href =
+                                "https://www.linkedin.com/in/alex-hughes-b960a1301/")
+                        }
+                    >
+                        <LinkedInIcon
+                            sx={{
+                                fontSize: {
+                                    xs: "60px",
+                                    sm: "80px",
+                                    md: "100px",
+                                    lg: "120px",
+                                    xl: "140px",
+                                },
+                            }}
+                        />
+                    </IconButton>
+                    <IconButton
+                        aria-label="redirect"
+                        onClick={(event) =>
+                            (window.location.href =
+                                "https://github.com/al-ex-huze")
+                        }
+                    >
+                        <GitHubIcon
+                            sx={{
+                                fontSize: {
+                                    xs: "60px",
+                                    sm: "80px",
+                                    md: "100px",
+                                    lg: "120px",
+                                    xl: "140px",
+                                },
+                            }}
+                        />
+                    </IconButton>
+                    <IconButton
+                        aria-label="redirect"
+                        onClick={(event) =>
+                            (window.location.href =
+                                "mailto:al.ex.hughes@outlook.com")
+                        }
+                    >
+                        <EmailIcon
+                            sx={{
+                                fontSize: {
+                                    xs: "60px",
+                                    sm: "80px",
+                                    md: "100px",
+                                    lg: "120px",
+                                    xl: "140px",
+                                },
+                            }}
+                        />
+                    </IconButton>
+                </CardActions>
+                <Typography
+                    sx={{
+                        color: "text.secondary",
+                        mb: 1.5,
+                    }}
+                >
+                    {"("}#Open To Work{")"}
+                </Typography>
+            </Card>}
+            />
         </Box>
     );
 };
