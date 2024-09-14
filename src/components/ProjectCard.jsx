37fb53f42d63982bf229f0a45e796c4d22092bde
Alex Hughes <al.ex.hughes@outlook.com>
2024-09-14 14:52:27 +0100

    updates theme, card, menu

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,6 @@ import styles from "./ProjectCard.module.css";
 
 import * as React from "react";
 
-import { Box } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
